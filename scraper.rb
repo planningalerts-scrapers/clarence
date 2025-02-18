123ca10222db8a6a7df33bc11b4a58a2e156fc96
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-02-19 05:40:18 +1100

    Update to reflect new HTML class names and use of en-dash.
    Handle where a minus rather than en-dash is used as the last separator.

--- scraper.rb
+++ scraper.rb
@@ -3,19 +3,25 @@ require 'mechanize'
 
 a = Mechanize.new
 
+EN_DASH = "\u2013"
+
 url = "https://www.ccc.tas.gov.au/planning-development/planning/advertised-planning-permit-applications/"
 a.get(url) do |page|
-  page.search('.doc-list a').each do |a|
+  page.search('.content-card__title a').each do |a|
     unless a.at('img')
-      # Long winded name of PDF
+      # Long-winded name for map link
       name = a.inner_text.strip
-      s = name.split(' - ').map(&:strip)
+      s = name
+            .sub("- Advertising period expires", "#{EN_DASH} Advertising period expires")
+            .split("#{EN_DASH} ")
+            .map(&:strip)
       # Skip over links that we don't know how to handle
       if s.count != 4
-        puts "Unexpected form of PDF name. So, skipping: #{name}"
+        puts "WARNING: Skipping map link with #{s.count} rather than 4 parts: #{s.inspect}"
         next
       end
 
+      puts "Found: #{s.inspect}"
       record = {
         'council_reference' => s[0],
         'address' => s[1] + ", TAS",
