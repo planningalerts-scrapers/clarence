b3f1b112eda1ffba92b747e3cc8ef906135d08fc
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-07-06 21:01:20 +1000

    Run rubocop -a

--- Gemfile
+++ Gemfile
@@ -2,9 +2,9 @@ source "https://rubygems.org"
 
 ruby "3.2.2"
 
-gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git", branch: "morph_defaults"
 gem "mechanize", "~> 2.8.5"
 gem "nokogiri", "~> 1.15.0"
+gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git", branch: "morph_defaults"
 gem "sqlite3", "~> 1.6.3"
 
 # Development and Test
--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,5 @@
-require 'scraperwiki'
-require 'mechanize'
+require "scraperwiki"
+require "mechanize"
 
 a = Mechanize.new
 
@@ -7,17 +7,17 @@ EN_DASH = "\u2013"
 
 url = "https://www.ccc.tas.gov.au/planning-development/planning/advertised-planning-permit-applications/"
 a.get(url) do |page|
-  page.search('.content-card__inner').each do |card|
-    title_link = card.at('.content-card__title a')
+  page.search(".content-card__inner").each do |card|
+    title_link = card.at(".content-card__title a")
     pdf_link = card.at('.content-card__buttons a[href$=".pdf"]')
-    next if title_link.nil? || pdf_link.nil? || title_link.at('img')
+    next if title_link.nil? || pdf_link.nil? || title_link.at("img")
 
     # Long-winded name for map link has what we need
     name = title_link.inner_text.strip
     s = name
-          .sub("- Advertising period expires", "#{EN_DASH} Advertising period expires")
-          .split("#{EN_DASH} ")
-          .map(&:strip)
+        .sub("- Advertising period expires", "#{EN_DASH} Advertising period expires")
+        .split("#{EN_DASH} ")
+        .map(&:strip)
 
     # Skip over links that we don't know how to handle
     if s.count != 4
@@ -28,22 +28,22 @@ a.get(url) do |page|
     puts "Found: #{s.inspect}"
     begin
       on_notice_to = begin
-                       Date.parse(s[3].sub('Advertising period expires ', ''))
-                     rescue ArgumentError
-                       nil
-                     end
+        Date.parse(s[3].sub("Advertising period expires ", ""))
+      rescue ArgumentError
+        nil
+      end
       description = "#{s[2]}#{on_notice_to ? '' : "; #{s[3]}"}"
       record = {
-        'council_reference' => s[0],
-        'address' => s[1] + ", TAS",
-        'description' => description,
-        'on_notice_to' => on_notice_to.to_s,
-        'date_scraped' => Date.today.to_s,
-        'info_url' => pdf_link['href']
+        "council_reference" => s[0],
+        "address" => s[1] + ", TAS",
+        "description" => description,
+        "on_notice_to" => on_notice_to.to_s,
+        "date_scraped" => Date.today.to_s,
+        "info_url" => pdf_link["href"],
       }
 
-      ScraperWiki.save_sqlite(['council_reference'], record)
-    rescue => e
+      ScraperWiki.save_sqlite(["council_reference"], record)
+    rescue StandardError => e
       puts "  Ignored erroneous record: #{e.class.name}: #{e.message}"
     end
   end
